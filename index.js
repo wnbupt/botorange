e9fd2cd12f6ac9d7795fbfdde3ca60b649a3b21b
lijiarui <ruiruibupt@126.com>
2017-07-06 02:08:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = new express();
+
+app.use(express.static('./view'));
+
+const port = 8080;
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './view/index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.info(`Server running on http://localhost:${port}`)
+});
